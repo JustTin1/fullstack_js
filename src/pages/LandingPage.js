d6ae8e9b68a39f2bac37619a7fe954216d5673da
Martin Halawa <martinhalawa12345@gmail.com>
2024-06-02 00:48:53 +0700

    hero 1.0

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,15 @@
 import React, { Component} from "react";   
 
 import Header from "./parts/Header";
-export default class LandingPage extends Component {
+import Hero from "./parts/Hero";
+
+import LandingPage from 'json/landingPage.json'
+export default class LandingPageData extends Component {
     render() {
         return (
           <>
             <Header {...this.props}></Header>
+            <Hero data={LandingPage.hero}/>
           </>  
         )
     }
