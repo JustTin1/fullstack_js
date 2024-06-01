392695d910ed27667f0cd0f9230822bda1b9dc02
Martin Halawa <martinhalawa12345@gmail.com>
2024-06-01 17:54:32 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-import "assets/scss/style.scss";
+
+import './assets/scss/style.scss';
+
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return (
-    <div className="App">Hello World</div>
-  );
+  return <div className="App">
+    <Router>
+      <Route path="/" Component={LandingPage}></Route>
+    </Router>
+  </div>
+  
 }
 
 export default App;
