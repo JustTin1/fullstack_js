b27a1305b7f6cd20fce354a6453e70d76406524a
Martin Halawa <martinhalawa12345@gmail.com>
2024-06-03 14:49:09 +0700

    mostpicked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component} from "react";
 
 import Header from "./parts/Header";
 import Hero from "./parts/Hero";
+import MostPicked from "./parts/MostPicked";
 
 import LandingPage from 'json/landingPage.json'
 export default class LandingPageData extends Component {
@@ -10,6 +11,7 @@ export default class LandingPageData extends Component {
           <>
             <Header {...this.props}></Header>
             <Hero data={LandingPage.hero}/>
+            <MostPicked data={LandingPage.mostPicked}/>
           </>  
         )
     }
